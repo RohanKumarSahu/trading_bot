a5839db4f5d553c2d30e386d3ebb14bb48761023
Rohan Kumar <rohan.rks25@gmail.com>
2021-03-02 17:50:59 +0530

    reading file

--- webhook/views.py
+++ webhook/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+import os
 import json
-import csv
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
@@ -17,11 +17,17 @@ from rest_framework.response import Response
 def webhooks(request):
     jsondata = request.body
     data = json.loads(jsondata)
-    json_dump = json.dumps(data)
-    print(json_dump)
+    file_location = os.listdir('./asset/data.json')
+    
+    with open(file_location, 'w') as json_file:
+        json.dump(data, json_file)
+    # print(json_dump)
+    file = open(file_location, 'r')
+    print("FROM FILE" + file.read())
+
 
     response = HttpResponse(json_dump, content_type='text/csv')
-    print(response)
+    #print(response)
     response['Content-Disposition'] = 'attachment; filename="data.csv"'
     return response
     return HttpResponse(status=200)
