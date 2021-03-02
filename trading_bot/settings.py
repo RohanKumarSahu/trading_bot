8ece48f251d3507a80dcef65d26573ddfa1765f3
Rohan Kumar <rohan.rks25@gmail.com>
2021-03-02 22:24:10 +0530

    template

--- trading_bot/settings.py
+++ trading_bot/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'trading_bot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
